d5bb253df6e0ab0b8a0ade63b4ebea9868bad536
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-06-15 14:25:31 -0700

    added google meta tag in index.html

--- src/components/map.js
+++ src/components/map.js
@@ -23,7 +23,7 @@ class Map extends React.Component {
   render() {
     return (
     <MapWithAMarker
-    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVzd2XPl8f7NZk1PN03mzAC7aI1ybumLM&v=3.exp&libraries=geometry,drawing,places"
     loadingElement={<div style={{ height: `100%` }} />}
     containerElement={<div style={{ height: `400px` }} />}
     mapElement={<div style={{ height: `100%` }} />}

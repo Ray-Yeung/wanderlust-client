417d1f70893e36885764475ed92d0e86bcc097ee
JacobAZimmerman <ijacobzim@gmail.com>
2018-06-18 16:40:12 -0700

    created initial fetch photo

--- src/actions/results.js
+++ src/actions/results.js
@@ -20,6 +20,17 @@ export const fetchDetailsSuccess = (details) => ({
     details
 });
 
+export const FETCH_PHOTO_ERROR = 'FETCH_PHOTO_ERROR';
+export const fetchPhotoError = () => ({
+    type: FETCH_PHOTO_ERROR
+});
+
+export const FETCH_PHOTO_SUCCESS = 'FETCH_PHOTO_SUCCESS';
+export const fetchPhotoSuccess = (photo) => ({
+    type: FETCH_PHOTO_SUCCESS,
+    photo
+});
+
 export const fetchPlacesDetails = (placeId) => (dispatch) => {
     // to fetch:
     //geometry - location
@@ -34,6 +45,27 @@ export const fetchPlacesDetails = (placeId) => (dispatch) => {
         },
     })
     .then(response => response.json())
-    .then(data => dispatch(fetchDetailsSuccess(data.result)))
+    .then(data => {
+        dispatch(fetchDetailsSuccess(data.result))
+        // return data.result;
+    })
+    // .then(data => {
+    //     if (data.photos[0]) {
+    //         dispatch(fetchPhoto(data.photos[0].photo_reference));
+    //     }
+    //     else return
+    // })
     .catch(err => dispatch(fetchDetailsError(err)))
 };
+
+// export const fetchPhoto = (photoId) => (dispatch) => {
+//     return fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/photo?maxwidth=2000&photoreference=${photoId}&key=AIzaSyCVzd2XPl8f7NZk1PN03mzAC7aI1ybumLM`, {
+//         method: 'GET',
+//         headers: {
+//           'Content-Type': 'image/jpeg'
+//         },
+//     })
+//     .then(response => response)
+//     .then(data => console.log(data))
+//     .catch(err => dispatch(fetchPhotoError()))
+// }

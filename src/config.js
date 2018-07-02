c808baed4229f4a70c4508b9312a5114046bc7a4
JacobAZimmerman <ijacobzim@gmail.com>
2018-07-02 13:56:07 -0700

    env vars

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000
 
 export const IMAGE_KEY = process.env.IMAGE_KEY
 
-export const GOOGLE_API_KEY = process.env.GOOGLE_MAPS_API_KEY
+export const GOOGLE_API_KEY = process.env.GOOGLE_API
 
 console.log('key',GOOGLE_API_KEY);
-console.log('entry key',process.env.GOOGLE_MAPS_API_KEY);
+console.log('entry key',process.env.GOOGLE_API);

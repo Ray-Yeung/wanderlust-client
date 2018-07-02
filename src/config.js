3b6ab029469b5fdbc1982bde52a5b17aaed8edaa
JacobAZimmerman <ijacobzim@gmail.com>
2018-07-02 14:12:08 -0700

    removed logs

--- src/config.js
+++ src/config.js
@@ -6,7 +6,3 @@ export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000
 export const IMAGE_KEY = process.env.IMAGE_KEY
 
 export const GOOGLE_API_KEY = process.env.REACT_APP_API_GOOGLE
-
-console.log('key',GOOGLE_API_KEY);
-console.log('entry key',process.env.REACT_APP_API_GOOGLE);
-console.log('base url', API_BASE_URL);
